646f1f8c2749f3fc0cae3f780e0ce4e11f93c57d
Anton Zdanov <azdanov@pm.me>
2019-05-06 15:54:42 +0300

    feat(project): init

---
+++ src/main/java/com/azdanov/pawatask/PawaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.azdanov.pawatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawaTaskApplication.class, args);
+    }
+
+}
