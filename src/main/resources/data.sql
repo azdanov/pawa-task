1c6b7ba0b9381ec68c6a0aa2b4157b234593eefe
Anton Zdanov <azdanov@pm.me>
2019-05-06 17:36:50 +0300

    feat(resources): seed data on each restart

---
+++ src/main/resources/data.sql
@@ -0,0 +1,31 @@
+INSERT IGNORE INTO task
+VALUES (1,
+        'drive dot-com channels',
+        'Nature picture challenge TV. Opportunity recently fight war. Book forward reduce charge.',
+        'NORMAL',
+        TRUE,
+        '2019-06-06 19:59:17.000000');
+INSERT IGNORE INTO task
+VALUES (2,
+        'whiteboard back-end channels',
+        'Campaign others team. Better base everything phone hope. According their leader.',
+        'HIGH',
+        TRUE,
+        '2019-05-09 05:52:17.000000');
+INSERT IGNORE INTO task
+VALUES (3,
+        'empower leading-edge e-commerce',
+        'Country must form house street when. Already speech finish around. Decade employee art attention physical.',
+        'LOW',
+        TRUE,
+        '2019-05-19 12:20:17.000000');
+
+INSERT IGNORE INTO comment
+VALUES (1, 'First', 1),
+       (2, 'Second', 1);
+INSERT IGNORE INTO comment
+VALUES (3, 'Third', 2),
+       (4, 'Fourth', 2);
+INSERT IGNORE INTO comment
+VALUES (5, 'Fifth', 3),
+       (6, 'Sixth', 3);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS task
+(
+    id          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255) DEFAULT NULL,
+    description VARCHAR(255) DEFAULT NULL,
+    priority    VARCHAR(255) DEFAULT NULL,
+    status      BIT(1)           NOT NULL,
+    due_date    DATETIME(6)  DEFAULT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    id          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(255) DEFAULT NULL,
+    task_id     INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE CASCADE
+) ENGINE = InnoDB;
