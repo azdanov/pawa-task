f709eb90b9d24d9bcf1a8d55fd920ae0a738e418
Anton Zdanov <azdanov@pm.me>
2019-05-07 19:44:04 +0300

    feat(frontend): finish design (add missing files)

---
+++ frontend/src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import mutations from "./mutations";
+import actions from "./actions";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    tasks: []
+  },
+  mutations,
+  actions
+});
---
+++ frontend/src/views/Tasks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="title">Tasks</h1>
+    <TaskList :tasks="tasks"></TaskList>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import TaskList from "@/components/TaskList";
+export default {
+  components: { TaskList },
+  computed: {
+    ...mapState(["tasks"])
+  },
+  created() {
+    this.$store.dispatch("fetchTasks");
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin: 0 0 1.5rem;
+  font-size: 3rem;
+  font-weight: lighter;
+  text-align: center;
+}
+</style>
