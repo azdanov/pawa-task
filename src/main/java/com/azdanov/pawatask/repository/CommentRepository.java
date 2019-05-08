380e5241bb7afb39c5b8b6b94273f1b1c69cfc4f
Anton Zdanov <azdanov@pm.me>
2019-05-08 16:16:58 +0300

    fix(backend): add Repository annotation

--- src/main/java/com/azdanov/pawatask/repository/CommentRepository.java
+++ src/main/java/com/azdanov/pawatask/repository/CommentRepository.java
@@ -2,5 +2,7 @@ package com.azdanov.pawatask.repository;
 
 import com.azdanov.pawatask.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {}
--- src/main/java/com/azdanov/pawatask/repository/TaskRepository.java
+++ src/main/java/com/azdanov/pawatask/repository/TaskRepository.java
@@ -2,5 +2,7 @@ package com.azdanov.pawatask.repository;
 
 import com.azdanov.pawatask.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {}
