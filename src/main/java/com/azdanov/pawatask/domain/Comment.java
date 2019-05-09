589c7d16b6a7c09054df44c5e9614cda196a6e8f
Anton Zdanov <azdanov@pm.me>
2019-05-09 23:48:58 +0300

    feat(backend): add date to comment, update sql, rename description

--- src/main/java/com/azdanov/pawatask/domain/Comment.java
+++ src/main/java/com/azdanov/pawatask/domain/Comment.java
@@ -1,11 +1,14 @@
 package com.azdanov.pawatask.domain;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -18,10 +21,22 @@ public class Comment {
     private int id;
 
     @NotBlank
-    @Column(name = "description")
-    private String description;
+    @Column(name = "text")
+    private String text;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date")
+    private Date date;
 
     public Comment() {}
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
     public int getId() {
         return id;
@@ -31,11 +46,11 @@ public class Comment {
         this.id = id;
     }
 
-    public String getDescription() {
-        return description;
+    public String getText() {
+        return text;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setText(String text) {
+        this.text = text;
     }
 }
